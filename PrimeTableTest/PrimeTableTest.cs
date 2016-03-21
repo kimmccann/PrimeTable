a2790fb0f9ccda92380cebda26cc56583f82084e
kimmccann <kimberleylmccann@gmail.com>
2016-03-21 11:58:12 +0000

    Added test to check if we get the correct result for first 3 nuprimes

--- PrimeTableTest/PrimeTableTest.cs
+++ PrimeTableTest/PrimeTableTest.cs
@@ -16,5 +16,15 @@ namespace PrimeTableTest
             Assert.IsTrue(result.Contains(prime1));
         }
 
+        [TestMethod]
+        public void firstThreePrimes()
+        {
+            List<int> primesList = new List<int>();
+            primesList.Add(2);
+            primesList.Add(3);
+            primesList.Add(5);
+            List<int> result = PrimeTable.PrimeTable.getPrimes(3);
+            CollectionAssert.AreEqual(primesList, result);
+        }
     }
 }
